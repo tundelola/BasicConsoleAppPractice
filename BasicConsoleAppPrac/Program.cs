f4c33481bfcb6596d1fc6f7a465404bf6c8cfb5c
tundelola <foladayo@aol.com>
2016-12-21 18:10:04 -0500

    first commit

---
+++ BasicConsoleAppPrac/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicConsoleAppPrac
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //Write a C# Sharp program to print Hello and your name in a separate line 
+            // Console.WriteLine(12+13);
+            //// Console.WriteLine("Hello: Lola Alade");
+            //Console.WriteLine("Please enter any number of your choice");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //// int sum = sumNum(3, 4);
+            //int sum = sumNum(num, num1);
+            //Console.WriteLine("sum of the 2 numbers = {0}", sum);
+
+            //Write a C# Sharp program to print the result of dividing two numbers 
+            // Console.WriteLine(18/3);
+
+            //Write a C# Sharp program to print the result of the specified operations 
+            //-1 + 4 * 6 • ( 35+ 5 ) % 7 • 14 + -4 * 6 / 11 • 2 + 15 / 6 * 1 - 7 % 2 
+
+            //Console.WriteLine(-1 + 4 * 6 );
+            //Console.WriteLine((35 + 5) % 7);
+            //Console.WriteLine(14 + -4 * 6 / 11);
+            //Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+            // Write a C# Sharp program to swap two numbers
+
+            //Console.WriteLine("Please enter any number of your choice");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int temp = num1;
+            //num1 = num2;
+            //num2 = temp;
+
+
+            //  Console.WriteLine("num and num1 after swapping is now = {0}, {1}", num1, num2);
+
+            //Console.Write("\nAfter Swapping : "); Console.Write("\nFirst Number : " + num1);
+            //Console.Write("\nSecond Number : " + num2);
+            //Console.Read();
+
+            //// Write a C# Sharp program to print the output of multiplication of three numbers which will be entered by the user.
+            //Console.WriteLine("Please enter any number of your choice");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+
+            ////Console.WriteLine("The multiplication of the 3 numbers is {0}",num1*num2*num3);
+            ////Console.WriteLine(num1 * num2 * num3);
+
+            //int result = num1 * num2 * num3;
+            //Console.WriteLine("Output: {0} x {1} x {2} = {3}", num1, num2, num3, result);
+
+
+            ////Write a C# Sharp program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user. 
+            //Console.WriteLine("Please enter any number of your choice");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int adding = num1 + num2;
+            //int subtract = num1 - num2;
+            //int multiply = num1 * num2;
+            //int divide = num1/num2;
+            //int mod = num1%num2;
+
+            //Console.WriteLine("Result of adding 2 numbers = {0} + {1} = {2}", num1, num2, adding);
+            //Console.WriteLine("Result of subtrating 2 numbers = {0} - {1} = {2}", num1, num2, subtract);
+            //Console.WriteLine("Result of multipling 2 numbers = {0} * {1} = {2}", num1, num2, multiply);
+            //Console.WriteLine("Result of deviding 2 numbers = {0}/{1} = {2}", num1, num2, divide);
+            //Console.WriteLine("Result of modular of 2 numbers = {0}/{1} = {2}", num1, num2, mod);
+
+            //Write a C# Sharp program that takes a number as input and print its multiplication table 
+            // int result;
+
+            // Console.WriteLine("Please enter any number of your choice");
+            // int num1 = Convert.ToInt32(Console.ReadLine());
+
+            // result = num1 * 1;
+            // //Console.WriteLine(result);
+
+            // Console.WriteLine("Result of multipling 2 numbers = {0} * {1} = {2}", num1, 1, result); 
+
+            // result = num1 * 2;
+
+            //Console.WriteLine("                             = {0}*{1} = {2}", num1, 2,  result);
+            // result = num1 * 3;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 3, result);
+
+            // result = num1 * 4;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 4, result);
+
+            // result = num1 * 5;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 5, result);
+
+            // result = num1 * 6;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 6, result);
+
+            // result = num1 * 7;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 7, result);
+
+            // result = num1 * 8;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 8, result);
+
+            // result = num1 * 9;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 9, result);
+
+            // result = num1 * 10;
+
+            // Console.WriteLine("                             = {0}*{1} = {2}", num1, 10, result);
+
+
+
+
+            // Write a C# Sharp program that takes four numbers as input to calculate and print the average. 
+            //Console.WriteLine("Please enter any number of your choice");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter 2nd number of your choice");
+            //int num4 = Convert.ToInt32(Console.ReadLine());
+
+            // int average = (num1 + num2 + num3 + num4) /4;
+            //// int count = 4;
+
+
+
+            // Console.WriteLine("The average of the 4 numbers = {0}, {1}, {2}, {3} = {4}", num1, num2, num3, num4,  average);
+
+            //   int result = (num1 + num2 + num3 + num4) / 4;
+            //   Console.WriteLine("The average of {0} , {1} , {2} , {3} is: {4} ",
+            //num1, num2, num3, num4, result);
+
+            // Write a C# Sharp program to that takes three numbers(x,y,z) as input and print the output of (x+y)·z and x·y + y·z
+            int result;
+            Console.WriteLine("Please enter any number of your choice");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 2nd number of your choice");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter 3rd number of your choice");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //result = (num1 + num2) * num3;
+            //Console.WriteLine("The result of ({0} + {1}) * {2} is: {3} ", num1, num2, num3, result);
+
+
+            result = num1 * num2 + num2 * num3;
+
+            Console.WriteLine("The result of ({0} * {1}) +{1} *{2} is: {3} ", num1, num2, num3, result);
+
+
+
+
+
+            //   //public static int sumNum(int num, int num1)
+            //{
+            //    return num + num1;
+
+
+            //}
+        }
+    }
+}
